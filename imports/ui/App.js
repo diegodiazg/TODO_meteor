eb9e00482f1605d26903ce6fa5d24be4873f4c98
diego diaz <remedios.paraelalma@gmail.com>
2017-12-13 11:54:49 -0600

    FTR integracion TreeBread

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,36 +1,88 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
+import {Treebeard} from 'react-treebeard';
 
 import { withTracker } from 'meteor/react-meteor-data';
 import ReactDOM from 'react-dom';
 import { Tasks } from '../api/tasks.js';
+import * as filters from './filter';
 
 import Task from './Task.js';
 import AccountsUIWrapper from './AccountsUIWrapper.js';
+const data = {
+    name: 'root',
+    toggled: true,
+    children: [
+        {
+            name: 'parent',
+            children: [
+                { name: 'child1' },
+                { name: 'child2' }
+            ]
+        },
+        {
+            name: 'loading parent',
+            loading: true,
+            children: []
+        },
+        {
+            name: 'parent',
+            children: [
+                {
+                    name: 'nested parent',
+                    children: [
+                        { name: 'nested child 1' },
+                        { name: 'nested child 2' }
+                    ]
+                }
+            ]
+        }
+    ]
+};
 
 
 // App component - represents the whole app
 class App extends Component {
-  constructor(props) {
-    super(props);
- 
-    this.state = {
-      hideCompleted: false,
-    }; 
-}
+    constructor(props) {
+        super(props);
+     
+        this.state = {
+          hideCompleted: false,
+        }; 
+        this.onToggle = this.onToggle.bind(this);
+    }
+
+    onToggle(node, toggled){
+        if(this.state.cursor){this.state.cursor.active = false;}
+        node.active = true;
+        if(node.children){ node.toggled = toggled; }
+        this.setState({ cursor: node });
+    }
+
+
 
     handleSubmit(event) {
         event.preventDefault(); 
         // Find the text field via the React ref
-        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+        const name = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
         const father ="";
-        Tasks.insert({
+       /* Tasks.insert({
             text,
             createdAt: new Date(), // current time
             owner: Meteor.userId(),           // _id of logged in user
             username: Meteor.user().username,  // username of logged in user
             father
-        });
+        });*/
+        Tasks.insert({
+            name,
+            children:[],
+            active:true,
+            createdAt: new Date(), // current time
+            owner: Meteor.userId(),           // _id of logged in user
+            username: Meteor.user().username,  // username of logged in user
+                    
+         });
+
         // Clear form
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
@@ -39,6 +91,15 @@ class App extends Component {
         this.setState({
             hideCompleted: !this.state.hideCompleted,
         });
+    }
+      onFilterMouseUp(e) {
+        const filter = e.target.value.trim();
+        if (!filter) {
+            return this.setState({data});
+        }
+        var filtered = filters.filterTree(data, filter);
+        filtered = filters.expandFilteredNodes(filtered, filter);
+        this.setState({data: filtered});
     }
 
     renderTasks() {
@@ -50,6 +111,9 @@ class App extends Component {
             <Task key={task._id} task={task} />
         ));
     }
+      
+         
+  
 
     render() {
         return (
@@ -77,7 +141,23 @@ class App extends Component {
                 <ul>
                     {this.renderTasks()}
                 </ul>
+                <div >
+                    <div className="input-group">
+                        <span className="input-group-addon">
+                          <i className="fa fa-search"/>
+                        </span>
+                        <input className="form-control"
+                               onKeyUp={this.onFilterMouseUp.bind(this)}
+                               placeholder="Search the tree..."
+                               type="text"/>
+                    </div>
+                </div>
+                <Treebeard
+                data={data}
+                onToggle={this.onToggle}
+            />
             </div>
+
             );
         }
     }
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Tasks } from '../api/tasks.js';
 import ReactDOM from 'react-dom';
+import {Treebeard} from 'react-treebeard';
 
 // Task component - represents a single todo item
 export default class Task extends Component {
@@ -18,6 +19,23 @@ updateTask() {
     });
   }
 
+
+  ChildTask() {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this.props.task._id, {
+        $set: { children: [{
+            name: ReactDOM.findDOMNode(this.refs.textTask).value.trim(),
+            children: [],
+            active: true
+        }]},
+      });
+    }
+
+  addItem() {
+    // Set the checked property to the opposite of its current value
+    ///content_item
+  }
+
  
   deleteThisTask() {
     Tasks.remove(this.props.task._id);
@@ -27,7 +45,7 @@ updateTask() {
   	    const taskClassName = this.props.task.checked ? 'checked' : '';
 
     return (
-    	<li className={taskClassName}>
+    	<li className={taskClassName} onClick={this.addItem.bind(this)}>
               <button className="delete" onClick={this.deleteThisTask.bind(this)}>
                 &times;
               </button>
@@ -40,8 +58,9 @@ updateTask() {
               />
        
               <span className="text">
-             <strong>{this.props.task.username}</strong>: <input ref="textTask" onChange={this.updateTask.bind(this) } type="text" value={this.props.task.text} />
+             <strong>{this.props.task.username}</strong>: <input ref="textTask" onChange={this.updateTask.bind(this) } type="text" value={this.props.task.name} />
               </span>
+              
             </li>
     );
   }
